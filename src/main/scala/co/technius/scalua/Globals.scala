573fc60d34c783ad1962a33b1a7cf979664c60af
Technius <techniux@gmail.com>
2014-11-05 17:34:48 -0800

    Add Globals wrapper

---
+++ src/main/scala/co/technius/scalua/Globals.scala
@@ -0,0 +1,33 @@
+package co.technius.scalua
+
+import java.io.{InputStream, PrintStream, StringReader}
+import org.luaj.{vm2 => luaj}
+import org.luaj.vm2.lib.jse.JsePlatform
+import scala.io.Source
+
+class Globals(override val wrapped: luaj.Globals) extends LuaValue(wrapped) {
+  def stdin: InputStream = wrapped.STDIN
+  def stdout: PrintStream = wrapped.STDOUT
+  def stderr: PrintStream = wrapped.STDERR
+
+  def stdin_=(value: InputStream): Unit = wrapped.STDIN = value
+  def stdout_=(value: PrintStream): Unit = wrapped.STDOUT = value
+  def stderr_=(value: PrintStream): Unit = wrapped.STDERR = value
+
+  def load(source: Source, chunkName: String) = {
+    val lines = source.getLines.mkString("\n")
+    source.close()
+    wrapped.load(new StringReader(lines), chunkName)
+  }
+
+  def load(script: String) = wrapped.load(script)
+
+  def load(script: String, chunkName: String) = wrapped.load(script, chunkName)
+
+  def loadFile(fileName: String) = wrapped.loadfile(fileName)
+}
+
+object Globals {
+  def standard() = JsePlatform.standardGlobals()
+  def debug() = JsePlatform.debugGlobals()
+}
