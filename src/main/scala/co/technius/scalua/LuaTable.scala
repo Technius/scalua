c9f6e0197b3df008895f240d60621f3092dd8019
Technius <techniux@gmail.com>
2014-11-08 11:51:51 -0800

    Implement LuaTable.toMap

--- src/main/scala/co/technius/scalua/LuaTable.scala
+++ src/main/scala/co/technius/scalua/LuaTable.scala
@@ -1,12 +1,26 @@
 package co.technius.scalua
 
 import org.luaj.vm2.{LuaValue => LuajValue, LuaTable => LuajTable}
+import scala.collection.mutable.ListBuffer
 import scala.language.implicitConversions
 
 class LuaTable(override val wrapped: LuajTable) extends LuaValue(wrapped) {
   def next(key: LuaValue): (LuaValue, LuaValue) = {
     val tmp = wrapped.next(key.wrapped)
     (LuaValue(tmp.arg1), LuaValue(tmp.arg(2)))
+  }
+
+  def toMap: Map[LuaValue, LuaValue] = {
+    val b = new ListBuffer[(LuaValue, LuaValue)]()
+    def mapImpl(current: LuaValue): Unit = next(current) match {
+      case (LuaNil, _) => Unit
+      case keypair => {
+        b += keypair
+        mapImpl(keypair._1)
+      }
+    }
+    mapImpl(LuaNil)
+    b.toMap
   }
 }
 
