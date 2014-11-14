478ea07631a1573f154c771090064a7cb27fd5a0
Technius <techniux@gmail.com>
2014-11-13 18:45:55 -0800

    Added basic Lua generator for case classes

--- src/main/scala/co/technius/scalua/LuaConverter.scala
+++ src/main/scala/co/technius/scalua/LuaConverter.scala
@@ -1,6 +1,12 @@
 package co.technius.scalua
 
+import scala.language.experimental.macros
+
 trait LuaConverter[T] {
   def toJava(value: LuaValue): Option[T] = None
   def toLua(value: T): LuaValue = ???
+}
+
+object LuaConverter {
+  def generate[T] = macro MacroImpl.generateConverterImpl[T]
 }
--- src/main/scala/co/technius/scalua/MacroImpl.scala
+++ src/main/scala/co/technius/scalua/MacroImpl.scala
@@ -19,4 +19,35 @@ object MacroImpl {
     }
     """)
   }
+
+  def generateConverterImpl[T: c.WeakTypeTag](c: Context): c.Expr[LuaConverter[T]] = {
+    import c.universe._
+    val typ = weakTypeOf[T]
+    val dec = typ.declarations
+    val methods = dec filter (_.isMethod) map (_.asMethod)
+    val caseValues = methods filter (_.isCaseAccessor)
+
+    val caseValuesAst = caseValues map { cv =>
+      q"""${cv.name.toString} -> LuaValue.toLua(value.${cv.name})"""
+    }
+
+    val ast = c.Expr[LuaConverter[T]](q"""new LuaConverter[$typ] {
+      override def toJava(value: LuaValue): Option[$typ] = value match {
+        case ud: LuaUserdata => {
+          val d = ud.data
+          if (d.isInstanceOf[$typ]) Some(d.asInstanceOf[$typ]) else None
+        }
+        case _ => None
+      }
+
+      override def toLua(value: $typ): LuaValue = {
+        val t = LuaTable(..$caseValuesAst)
+        t("__index") = t
+        LuaUserdata(value, t)
+      }
+    }
+    """)
+    showRaw(ast)
+    ast
+  }
 }
